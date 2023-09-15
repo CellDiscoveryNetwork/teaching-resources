a9cc0d7d88fa72c305d9173c3f04b5966e1d8f96
MarcElosua <elosua.marc@gmail.com>
2023-09-15 14:51:11 -0400

    update gene signatures

--- gene-signatures-1/gs-conda.sh
+++ gene-signatures-1/gs-conda.sh
@@ -2,11 +2,17 @@
 
 # Create the conda environment
 conda create --name gs python=3.10 -y
-which pip3 # Check I'm using the right pip
+conda activate gs
+# Check I'm using the right pip
+which pip3
 pip3 install decoupler
-conda install -c -y conda-forge plotnine
-conda install -c -y conda-forge scanpy 
-
+conda install -c conda-forge plotnine -y
+conda install -c conda-forge scanpy -y
+# Some conflicts may arise when installing jupyter, let conda handle it and wait for them to be resolved!
+conda install -c conda-forge jupyter
+conda install -c conda-forge ipykernel
+conda install -c conda-forge -c bioconda decoupler
+conda install -c conda-forge omnipath 
 # Create a kernel to use with Jupyter Notebooks in VSCode
 # https://medium.com/@nrk25693/how-to-add-your-conda-environment-to-your-jupyter-notebook-in-just-4-steps-abeab8b8d084
 python -m ipykernel install --user --name=gs
